6eab04179e164135019249a2f3ac1b4f60a413f5
Madhusudhan Gharti <madhusudhangharti.108@gmail.com>
2026-01-30 01:39:20 +0545

    add simulation loop and import libraries

--- code/coin-simulation.py
+++ code/coin-simulation.py
@@ -1,7 +1,8 @@
-
+#import libraries
+import random
 
 # Parameters 
-N = input ("Total number of tosses: ")
+N = int (input ("Total number of tosses: "))
 p = .5 # initial probability of heads
 alpha = .05 #significance level
 k = 5 # update interval
@@ -12,3 +13,30 @@ recent_outcomes = []
 head_count = 0
 tail_count = 0
 p_history = []
+
+#Simulation loop
+for i in range(N):
+    random_number = random.random() # Generates a random float between 0.0 and 1.0
+
+    if random_number <= p:
+        outcome = 1 # heads
+        head_count += 1
+    else:
+        outcome = 0 # tails
+        tail_count += 1
+
+    recent_outcomes.append(outcome)
+    
+    if len(recent_outcomes) == window:
+        head_count = recent_outcomes.count(1)
+        tail_count = recent_outcomes.count(0)
+        
+        if head_count > tail_count:
+            p = p + alpha
+        elif head_count < tail_count:
+            p = p - alpha
+        else:
+            pass
+
+        p_history.append(p)
+        recent_outcomes.clear()
